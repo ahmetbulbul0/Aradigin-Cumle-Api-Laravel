78b7e042230bce6a45c4799a1f7cbcfe889a7bf7
Ahmet Bülbül <ahmetbulbul@protonmail.com>
2022-10-26 17:37:57 +0300

    pagination added to limit generator, paginate generator created, pagination added to api categories controller

--- app/Http/Tools/LimitGenerator.php
+++ app/Http/Tools/LimitGenerator.php
@@ -19,4 +19,26 @@ class LimitGenerator extends Controller
         $data = $data->get();
         return $data;
     }
+
+    static function generateLimitAndPaginate($request, $data)
+    {
+        if ($request->limit) {
+            $limit = $request->limit;
+            $limit = intval($limit);
+            if ($limit != 0) {
+                $limit = $limit;
+                if ($request->page) {
+                    $page = intval($request->page);
+                    if ($page == 0) {
+                        $page = 1;
+                    }
+                } else {
+                    $page = 1;
+                }
+                $data = PaginateGenerator::paginate($data, $page, $limit);
+            }
+        }
+        return $data;
+    }
+
 }
